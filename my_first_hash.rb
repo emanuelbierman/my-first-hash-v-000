9bb0b20ec7ecbc12b3b62779563dc08f456fbea6
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-08 00:05:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,10 +20,9 @@ def retrieval
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
-                 }
-
+                }
+                
   shipping_manifest["oil paintings"]
-
 end
 
 def adding
